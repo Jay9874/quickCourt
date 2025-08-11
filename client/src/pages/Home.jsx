80b138d88af3c181458a78c7cc206ee180b45304
Jay9874 <jayprakashsharma225@gmail.com>
2025-08-11 14:46:05 +0530

    add home bg

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,18 +2,63 @@ import React from 'react'
 
 export default function Home () {
   return (
-    <div>
-      {/* the search box */}
-      <form>
-        <input type='text' name='city' placeholder='Ahmedabad' />
-      </form>
+    <div className='flex'>
+      {/* the bg container */}
       <div>
-        <h2>Find Players and Venue Nearby</h2>
-        <p>
-          Seamlessly explore special venue to play with
-          <br />
-          sports enthuasts just like you!
-        </p>
+
+      </div>
+      {/* the left container*/}
+      <div className='flex justify-center items-center'>
+        <form>
+          <div className='flex p-1 border border-2 rounded-md w-fit'>
+            <div style={{ height: '24px', width: '24px' }}>
+              <svg
+                viewBox='0 0 24 24'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'
+                stroke='#707070'
+              >
+                <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+                <g
+                  id='SVGRepo_tracerCarrier'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                ></g>
+                <g id='SVGRepo_iconCarrier'>
+                  {' '}
+                  <path
+                    d='M12 21C15.5 17.4 19 14.1764 19 10.2C19 6.22355 15.866 3 12 3C8.13401 3 5 6.22355 5 10.2C5 14.1764 8.5 17.4 12 21Z'
+                    stroke='#9c9c9c'
+                    stroke-width='2'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  ></path>{' '}
+                  <path
+                    d='M12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12Z'
+                    stroke='#9c9c9c'
+                    stroke-width='2'
+                    stroke-linecap='round'
+                    stroke-linejoin='round'
+                  ></path>{' '}
+                </g>
+              </svg>
+            </div>
+            <input
+              className=''
+              type='text'
+              name='city'
+              placeholder='Ahmedabad'
+            />
+          </div>
+        </form>
+        <div>
+          <h2>Find Players and Venue Nearby</h2>
+          <p>
+            Seamlessly explore special venue to play with
+            <br />
+            sports enthuasts just like you!
+          </p>
+        </div>
       </div>
     </div>
   )
